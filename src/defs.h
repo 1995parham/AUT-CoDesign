93ad283f3831f3f350ff85703740f306b01d1604
Parham Alvani <parham.alvani@gmail.com>
2016-03-06 22:12:33 +0330

    Problem definitions were added.

---
+++ src/defs.h
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : defs.h
+ *
+ * [] Creation Date : 06-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdint.h>
+
+typedef uint8_t kromosom[16];
+
+extern kromosom population[32];
+
+#endif
