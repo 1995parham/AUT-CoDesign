4f73d1cc23b004f1637637df0a1c89b2ae903482
Parham Alvani <parham.alvani@gmail.com>
2016-03-06 22:12:52 +0330

    Main added just for test.

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 06-03-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#include "defs.h"
+
+int main(int argc, char *argv[])
+{
+}
