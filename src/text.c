8f5856b3b4ee93f1607e73ed978029704b28780a
Parham Alvani <parham.alvani@gmail.com>
2016-04-10 05:50:05 +0430

    Text related functions were added.

---
+++ src/text.c
@@ -0,0 +1,29 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : text.c
+ *
+ * [] Creation Date : 10-04-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#include "defs.h"
+#include "text.h"
+
+char text[TEXT_SIZE] = "--Hello World!--";
+
+void decode_text(kromosom k, char decoded_text[TEXT_SIZE])
+{
+	int i, j;
+
+	for (i = 0; i < (int)(TEXT_SIZE / 16); i++) {
+		for (j = 0; j < 16; j++) {
+			decoded_text[i * 16 + j] = text[i * 16 + k[j]];
+		}
+	}
+}
---
+++ src/text.h
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : text.h
+ *
+ * [] Creation Date : 10-04-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef TEXT_H
+#define TEXT_H
+
+void decode_text(kromosom k, char decoded_text[TEXT_SIZE]);
+
+#endif
