d1db27707e51b12b0c923152ed175802abb9ddbc
Parham Alvani <parham.alvani@gmail.com>
2016-04-21 10:35:27 +0430

    We read the keys from the file now :)

--- src/main.c
+++ src/main.c
@@ -16,12 +16,14 @@
 #include <inttypes.h>
 
 #include "defs.h"
+#include "population.h"
 
 int main(int argc, char *argv[])
 {
 	uint8_t s1, p1;
 	uint8_t s2, p2;
 	uint8_t s3, p3;
+	int i, j;
 
 	/* Seed and Polynomial of our LFSR1 :) */
 	scanf("%" SCNu8 "%" SCNu8, &s1, &p1);
@@ -32,4 +34,14 @@ int main(int argc, char *argv[])
 	/* Seed and Polynomial of our LFSR2 :) */
 	scanf("%" SCNu8 "%" SCNu8, &s3, &p3);
 	GLFSR_init(&lfsr3, p3, s3);
+
+	/* Getting the first population :) */
+	FILE *keys = fopen("keys", "r");
+	for (i = 0; i < 32; i++)
+		for (j = 0; j < 16; j++)
+			fscanf(keys, "%" SCNu8, &population[i][j]);
+
+	/* Simulation */
+	for (i = 0; i < 1024; i++)
+		population_next();
 }
